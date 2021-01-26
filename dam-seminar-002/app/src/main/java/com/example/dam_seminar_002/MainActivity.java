fac1a3983c01e27ed818ccf4361af0769d738c8f
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-26 16:07:01 +0200

    dam seminar 002 - revision

---
+++ dam-seminar-002/app/src/main/java/com/example/dam_seminar_002/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dam_seminar_002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab_add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab_add = findViewById(R.id.fab_main);
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
